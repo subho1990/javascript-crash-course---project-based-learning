d08c092f5ebe1c3a911093ec67e7a729a57fd15c
Subho Kansabanik <00015subha@gmail.com>
2026-02-26 19:52:05 +0530

    Add files via upload

---
+++ clockapp-v2.js
@@ -0,0 +1,15 @@
+now = new Date();
+
+localtime = now.toString();
+utctime = now.toGMTString();
+
+document.write("<b>Local Time : </b>" + localtime + "<br/>");
+document.write("<b>UTC Time : </b>" + utctime);
+
+hours = now.getHours();
+mins = now.getMinutes();
+secs = now.getSeconds();
+
+document.write("<h1>");
+document.write(hours + " : " + mins + " : " + secs);
+document.write("</h1>");
